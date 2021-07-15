4a24ad9512cb618529e3668da55125c4bc05bc95
茶馆主人 <maczh@qq.com>
2021-07-15 15:16:57 +0800

    将取随机数与utils隔离，本地处理，避免出现引用循环

--- traceutil.go
+++ traceutil.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"github.com/gin-gonic/gin"
 	"github.com/maczh/mgcache"
-	"github.com/maczh/utils"
+	"math/rand"
 	"runtime"
 	"strconv"
 	"time"
@@ -13,7 +13,7 @@ import (
 func PutRequestId(c *gin.Context) {
 	requestId := c.GetHeader("X-Request-Id")
 	if requestId == "" {
-		requestId = utils.GetRandomHexString(16)
+		requestId = getRandomHexString(16)
 	}
 	routineId := GetGID()
 	mgcache.OnGetCache("RequestId").Add(routineId, requestId, 5*time.Minute)
@@ -36,4 +36,19 @@ func GetGID() uint64 {
 	b = b[:bytes.IndexByte(b, ' ')]
 	n, _ := strconv.ParseUint(string(b), 10, 64)
 	return n
+}
+
+func generateRandString(source string, l int) string {
+	bytes := []byte(source)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+func getRandomHexString(l int) string {
+	str := "0123456789abcdef"
+	return generateRandString(str, l)
 }
